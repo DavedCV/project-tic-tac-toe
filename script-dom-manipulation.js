ce27cddfb1f08551a07ab37085cb5aa435c4331f
David Castrillón <97773641+Davedcod@users.noreply.github.com>
2023-01-30 19:16:24 -0500

    add event listeners to display based on the buttons interactions

--- script-dom-manipulation.js
+++ script-dom-manipulation.js
@@ -1,23 +1,27 @@
-function deactivateHumanButton(button){
-    button.classList.remove("selected");
-    const input = button.nextElementSibling;
-    input.classList.remove("active");
-    input.value = "";
+function deactivateHumanButton(button) {
+  button.classList.remove("selected");
+  const input = button.nextElementSibling;
+  input.classList.remove("active");
+  input.value = "";
 }
 
-function deactivateComputerButton(button){
-    button.classList.remove("selected");
-    const input = button.previousElementSibling;
-    input.value = "";
+function deactivateComputerButton(button) {
+  button.classList.remove("selected");
+  const input = button.previousElementSibling;
+  input.value = "";
 }
 
-
+const startingInterface = document.querySelector(".startingInterface");
+const gameInterface = document.querySelector(".gameInterface");
 const selectHumanPlayer = document.querySelectorAll(
   ".selectionForm button:nth-child(2)"
 );
 const selectComputerPlayer = document.querySelectorAll(
   ".selectionForm button:nth-child(4)"
 );
+const submitButton = document.querySelector(".submitButton");
+
+startingInterface.classList.add("active");
 
 selectHumanPlayer.forEach((buttonHuman) =>
   buttonHuman.addEventListener("click", (event) => {
@@ -40,3 +44,12 @@ selectComputerPlayer.forEach((buttonComputer) =>
     input.value = "computer";
   })
 );
+
+submitButton.addEventListener("click", (event) => {
+  const form = event.target.closest("form");
+  if (form.checkValidity()) {
+    event.preventDefault();
+    startingInterface.classList.remove("active");
+    gameInterface.classList.add("active");
+  }
+});
