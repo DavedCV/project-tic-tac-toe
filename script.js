8b19e512af8a493fa9f710dfdde49961fed6266c
David Castrillón <97773641+Davedcod@users.noreply.github.com>
2023-08-09 18:34:19 -0500

    - Created styles and some target selectors to appropiately display the sign of the current player in the hover effect of the board tiles, excluding those already selected tiles - Created the factory function for players - Creted the module that manages the general flow of the game - Created the function to appropiately update the styles for every player in the main layout

--- script.js
+++ script.js
@@ -16,13 +16,33 @@ const GameBoard = (() => {
   return { getPosition, setPosition, resetFields };
 })();
 
+const Player = (playerName, playerSign) => {
+  const sign = playerSign;
+  const name = playerName;
+
+  let tries = 0;
+
+  const getName = () => name;
+  const getSign = () => sign;
+  const getTries = () => tries;
+  const updateTries = () => tries++;
+
+  return { getSign, getTries, updateTries, getName };
+};
+
 const layoutController = (function () {
   const gameInitializerButton = document.querySelector(
       ".game-initializer .start-button",
     ),
-    signSelector = document.querySelector(".game-initializer .sign-selector");
+    signSelector = document.querySelector(".game-initializer .sign-selector"),
+    mainLayout = document.querySelector("main"),
+    turnInfo = mainLayout.querySelector(".turn-info"),
+    tiles = mainLayout.querySelectorAll(".tile");
 
-  let firstPlayerSign, secondPlayerSign;
+  let firstPlayerSign,
+    secondPlayerSign,
+    firstPlayerName = "",
+    secondPlayerName = "";
 
   signSelector.addEventListener("click", (e) => {
     if (e.target.id === "sign-x") {
@@ -37,10 +57,20 @@ const layoutController = (function () {
   gameInitializerButton.addEventListener("click", (e) => {
     if (firstPlayerSign === undefined) return;
 
+    const firstPlayer = gameManager.setPlayers(
+      firstPlayerName,
+      firstPlayerSign,
+      secondPlayerName,
+      secondPlayerSign,
+    );
+
+    updatePlayerData(firstPlayer);
+
     animateButton(e.target);
 
     setTimeout(() => {
       e.target.parentElement.classList.add("disabled");
+      mainLayout.classList.remove("disabled");
     }, 500);
   });
 
@@ -54,5 +84,46 @@ const layoutController = (function () {
     button.addEventListener("transitionend", removeTransition);
   };
 
+  const updatePlayerData = (player) => {
+    const signClass = player.getSign() === "o" ? "sign--o" : "sign--x";
+    const notSignClass = player.getSign() === "o" ? "sign--x" : "sign--o"
+
+    turnInfo
+      .querySelector(".turn-info-sign")
+      .setAttribute("class", `turn-info-sign ${signClass}`);
+    turnInfo.querySelector("p").textContent = player.getName();
+
+    tiles.forEach(tile => {
+      if (!tile.classList.contains("sign")){
+        if (tile.classList.contains(notSignClass)) tile.classList.remove(notSignClass);
+        tile.classList.add(signClass);
+      }
+    });
+  };
+
   return {};
+})();
+
+const gameManager = (function () {
+  let player1, player2, playerRound;
+  let round = 0;
+
+  const setPlayers = (firstName, firstSign, secondName, secondSign) => {
+    if (firstName === "") player1 = Player("HUMAN", firstSign);
+    else player1 = Player(firstName, firstSign);
+
+    if (secondName === "") player2 = Player("AI", secondSign);
+    else player2 = Player(secondName, secondSign);
+
+    firstMove();
+    return playerRound;
+  };
+
+  const firstMove = () => {
+    playerRound = Math.random() >= 0.5 ? player1 : player2;
+  };
+
+  const playRound = () => {};
+
+  return { setPlayers };
 })();
